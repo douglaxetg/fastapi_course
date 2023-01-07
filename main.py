04b2827b498c621bd4e3a8bc9aa8f5b0003f3b3d
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-07 18:16:03 -0400

    testing server app

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('prueba')
+from fastapi import FastAPI
+
+app = FastAPI()
+app.title = "My app with FastAPI"
+app.version = "0.0.1"
+
+@app.get('/', tags=['home'])
+def message():
+    return "Hello world"
