48f06a028e3150b8977e3fd161b9b901e71b685a
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-07 22:46:07 -0400

    correction in class validation

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ class Movie(BaseModel):
     id: Optional[int] = None
     title: str = Field(min_length=5, max_length=15)
     overview: str = Field(min_length=15, max_length=50)
-    year: int = Field(default=2000, le=2000)
-    rating: float = Field(default=10.0)
-    category: str = Field(default="Accion")
+    year: int = Field(le=2000)
+    rating: float = Field(ge=1, le=10)
+    category: str = Field(min_length=5, max_length=15)
     
     class Config:
         schema_extra = {
