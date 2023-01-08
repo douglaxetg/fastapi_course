b9d1580ebc7840f35b3764923214e047c5b8e08d
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-07 21:04:44 -0400

    get method, routes and parameters

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
+ 
 app = FastAPI()
 app.title = "My app with FastAPI"
 app.version = "0.0.1"
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    },
+    {
+        'id': 2,
+        'title': 'Avatar2',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...2",
+        'year': '2010',
+        'rating': 7.8,
+        'category': 'Acción2'
+    } 
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world"
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    for item in movies:
+        if item['id'] == id:
+            return item
+    return []
